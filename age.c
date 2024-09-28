81b2d646904e0f394dfa019ccf6d2d2b4fc8d45b
kapoorashutosh <ashutoshkapoor3000@gmail.com>
2024-09-28 11:27:41 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the age");
+    scanf("%d", &a);
+    printf("Age is %d", a);
+    return 0;
+}
---
+++ age2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter the age: ");
+    scanf("%d", &a);
+    printf("Age is %d\n", a); // Added format specifier %d and a newline character
+
+    return 0;
+}
